1bbd5ebcb3d15b4dae79823ff768072bcb58fee1
matined <nedzimateusz@gmail.com>
2022-02-01 19:53:17 +0100

    gui added

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
-import numpy
+import numpy as np
 import pickle
-import os
+from PIL import Image
+import tensorflow as tf
 
 
 def get_data() -> dict:
@@ -15,3 +16,35 @@ def get_data() -> dict:
     """
     data = pickle.load(open('data/data.pkl', 'rb'))
     return data
+
+
+def preprocess_image(filepath: str) -> tf.Tensor:
+    """
+    Process an image so it's ready for prediction.
+
+    Parameters
+    ----------
+    filepath : string
+        Path to the photo
+
+    Returns
+    -------
+    image : tf.Tensor
+        The image as a tensorflow tensor
+    """
+    image = Image.open(filepath)
+    image.resize((100, 100))
+    image = tf.cast(image, dtype=tf.float32)
+    return image
+
+
+def predict(model, photo):
+    photo = preprocess_image(photo)
+    predictions = model.predict(np.array([photo]))
+    result = np.argmax(predictions)
+    return result
+
+
+def load_model():
+    model = tf.keras.models.load_model('models/model_1.h5')
+    return model
